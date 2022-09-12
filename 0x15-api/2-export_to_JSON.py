e46caa65393af2bb1173c3d00b833c0af0c94f05
Noluyolo Ndungane <98300986+Noluyolo97@users.noreply.github.com>
2022-09-12 15:45:01 +0200

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-""" Queries REST API for employee info, exports to JSON
-    argv 1 = int employee ID
-"""
+"""Exports data in the JSON format"""
+import json
+import requests
+import sys
+
 if __name__ == "__main__":
-    import json
-    import requests as r
-    from sys import argv
+    id = sys.argv[1]
+    userUrl = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    todoUrl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
 
-    user_id = argv[1]
-    # Finds employee username by "id" param in /users/
-    name_q = r.get("https://jsonplaceholder.typicode.com/users/{}/"
-                   .format(user_id))
-    data = name_q.json()
-    username = data.get("username")
+    userData = requests.get(userUrl).json()
+    todo = requests.get(todoUrl).json()
 
-    # Finds employee tasks by "userID" param; /users/ & /todo/ are linked
-    url = "https://jsonplaceholder.typicode.com/users/1/todos/"
-    task_q = r.get(url, params={'userId': user_id})
-    data = task_q.json()
-
-    # Creates JSON object with request data, writes to .json file
-
-    json_dict = {}
-    json_list = []
-
-    for elem in data:
-        json_dict["task"] = elem.get("title")
-        json_dict["completed"] = elem.get("completed")
-        json_dict["username"] = username
-        json_list.append(json_dict)
-        json_dict = {}
-
-    json_data = {}
-    json_data[user_id] = json_list
-    with open(user_id + ".json", 'w') as jsonfile:
-        json.dump(json_data, jsonfile)
+    with open('{}.json'.format(id), 'w') as json_file:
+        tasks = []
+        for t in todo:
+            tasks.append({"task": t.get("title"),
+                          "completed": t.get("completed"),
+                          "username": userData.get("username")})
+        data = {"{}".format(id): tasks}
+        json.dump(data, json_file)
+        
